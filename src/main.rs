36aab2e00395af1f2b8fe3bc859dbd2b42e2f90b
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-04-10 17:09:42 +0530

    Add support for piping stdin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::{env, process};
+use std::{env, io::Read, process};
 
 use miette::Result;
-
-use formatjson;
 
 const BOLD_RED: &str = "\x1b[1;31m";
 const BOLD_GREEN: &str = "\x1b[1;32m";
@@ -15,22 +13,34 @@ fn main() -> Result<()> {
     // ignore executable name itself, we care about first arg: filepath
     args.next();
 
-    let filepath = args.next().unwrap_or_else(|| {
-        eprintln!(
-            "{}Error:{} usage: formatjson path/to/filename.json",
-            BOLD_RED, NORMAL
-        );
-        process::exit(2);
-    });
-
-    if let Err(error) = formatjson::format_json_file(&filepath) {
-        if let formatjson::FormatJsonError::InvalidSyntax(err) = error {
-            return Err(err.into());
-        } else {
-            eprintln!("{}Error:{} {}", BOLD_RED, NORMAL, error);
-            process::exit(1);
+    // Expect filepath as an arg, and print success message on finish
+    if let Some(filepath) = args.next() {
+        if let Err(error) = formatjson::format_json_file(&filepath) {
+            handle_error(error)?;
+        }
+        eprintln!("{}Success:{} formatted {}", BOLD_GREEN, NORMAL, filepath);
+    // Otherwise expect JSON provided through stdin, and print output on stdout
+    } else {
+        if let Err(error) = format_stdin() {
+            handle_error(error)?;
         }
     }
-    eprintln!("{}Success:{} formatted {}", BOLD_GREEN, NORMAL, filepath);
     Ok(())
+}
+
+fn format_stdin() -> Result<(), formatjson::FormatJsonError> {
+    let mut buffer = String::new();
+    std::io::stdin().lock().read_to_string(&mut buffer)?;
+    let formatted_json = formatjson::format_json(&buffer)?;
+    println!("{}", formatted_json);
+    Ok(())
+}
+
+fn handle_error(error: formatjson::FormatJsonError) -> Result<()> {
+    if let formatjson::FormatJsonError::InvalidSyntax(err) = error {
+        return Err(err.into());
+    } else {
+        eprintln!("{}Error:{} {}", BOLD_RED, NORMAL, error);
+        process::exit(1);
+    }
 }
